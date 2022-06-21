935fca93b2f69086e7da89d1012382b240a4437b
ruineka <ruineka@pop-os.localdomain>
2022-06-21 13:22:30 -0500

    More directory issues. we'll get this eventually. :D

--- holoiso-setup-amd.sh
+++ holoiso-setup-amd.sh
@@ -5,7 +5,7 @@ git clone -b oxp-90 https://github.com/ruineka/gamescope-onexplayer
 cd gamescope-onexplayer
 meson build/
 ninja -C build/
-sudo cp ~/gamescope-onexplayer/build/gamescope /usr/bin
+sudo cp ~/gamescope-onexplayer/gamescope-onexplayer/build/gamescope /usr/bin
 
 git clone -b service-test https://github.com/shadowblip/handygccs
 cd gamescope-onexplayer
