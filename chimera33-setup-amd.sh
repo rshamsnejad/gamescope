3f27f684a1d3179f1b63cbd468b844f11c7ca0da
ruineka <ruineka@pop-os.localdomain>
2022-06-26 02:55:44 -0500

    Added necessary bits for ChimeraOS.

--- chimera33-setup-amd.sh
+++ chimera33-setup-amd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo frzr-unlock
 sudo pacman -Sy base-devel gcc-libs glibc libcap.so=2-64 libdrm libliftoff.so=0-64 libpipewire-0.3.so=0-64 libvulkan.so=1-64 libwlroots.so=10-64 libx11 libxcomposite libxdamage libxext libxkbcommon.so=0-64 libxrender libxres libxtst libxxf86vm sdl2 vulkan-icd-loader wayland wayland-protocols wlroots xorg-server-xwayland git glslang meson ninja vulkan-headers pixman linux-api-headers mesa udev seatd libinput libxfixes libxcb xcb-util-wm libxi xcb-util-errors libglvnd xorgproto
 git clone -b oxp-90 https://github.com/ruineka/gamescope-onexplayer
 cd gamescope-onexplayer
@@ -11,4 +12,5 @@ sudo cp /usr/local/bin /usr/bin
 git clone https://github.com/ShadowBlip/HandyGCCS.git
 cd HandyGCCS
 sudo make install
+chimera-session gamepadui
 reboot
