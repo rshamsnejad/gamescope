cb2e933f8e608bcba2cbb25df184ccf730c2ee43
ruineka <ruinairas1992@gmail.com>
2022-06-28 12:01:27 -0500

    Update chimera33-setup-amd.sh

--- chimera33-setup-amd.sh
+++ chimera33-setup-amd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo frzr-unlock
 sudo pacman -Sy base-devel gcc-libs glibc libcap.so=2-64 libdrm libliftoff.so=0-64 libpipewire-0.3.so=0-64 libvulkan.so=1-64 libwlroots.so=10-64 libx11 libxcomposite libxdamage libxext libxkbcommon.so=0-64 libxrender libxres libxtst libxxf86vm sdl2 vulkan-icd-loader wayland wayland-protocols wlroots xorg-server-xwayland git glslang meson ninja vulkan-headers pixman linux-api-headers mesa udev seatd libinput libxfixes libxcb xcb-util-wm libxi xcb-util-errors libglvnd xorgproto
 git clone -b oxp-90 https://github.com/ruineka/gamescope-onexplayer
 cd gamescope-onexplayer
