25266197ff990d2f17945e54cf8731489c9d98c4
ruineka <ruinairas1992@gmail.com>
2022-07-03 16:14:27 -0500

    Update chimera33-setup-intel.sh

--- chimera33-setup-intel.sh
+++ chimera33-setup-intel.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-sudo pacman -Sy base-devel gcc-libs glibc libcap.so=2-64 libdrm libliftoff.so=0-64 libpipewire-0.3.so=0-64 libvulkan.so=1-64 libwlroots.so=10-64 libx11 libxcomposite libxdamage libxext libxkbcommon.so=0-64 libxrender libxres libxtst libxxf86vm sdl2 vulkan-icd-loader wayland wayland-protocols wlroots xorg-server-xwayland git glslang meson ninja vulkan-headers pixman linux-api-headers mesa udev seatd libinput libxfixes libxcb xcb-util-wm libxi xcb-util-errors libglvnd xorgproto
+sudo pacman -Sy base-devel gcc-libs glibc libcap.so=2-64 libdrm llvm-libs libliftoff.so=0-64 libpipewire-0.3.so=0-64 libvulkan.so=1-64 libwlroots.so=10-64 libx11 libxcomposite libxdamage libxext libxkbcommon.so=0-64 libxrender libxres libxtst libxxf86vm sdl2 vulkan-icd-loader wayland wayland-protocols wlroots xorg-server-xwayland git glslang meson ninja vulkan-headers pixman linux-api-headers mesa udev seatd libinput libxfixes libxcb xcb-util-wm libxi xcb-util-errors libglvnd xorgproto
 git clone -b another-experiment-intel https://github.com/ruineka/gamescope-onexplayer ~/gamescope-onexplayer/oxp-90-intel
 cd ~/gamescope-onexplayer/oxp-90-intel
 meson build/
 ninja -C build/
 sudo meson install -C build/ --skip-subprojects
-killall -9 gamescope 
+killall -9 gamescope
 sudo cp ~/gamescope-onexplayer/oxp-90-intel/build/gamescope /usr/bin
-sudo cp /usr/local/bin /usr/bin
+sudo cp /usr/local/bin/gamescope /usr/bin
 
 git clone https://github.com/ShadowBlip/HandyGCCS.git ~/HandyGCCS
 cd ~/HandyGCCS
