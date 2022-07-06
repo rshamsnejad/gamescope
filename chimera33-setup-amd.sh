dd9a99a63435b9b0c237048368f4e3f958ca2c1b
ruineka <ruinairas1992@gmail.com>
2022-07-06 12:00:02 -0500

    Update chimera33-setup-amd.sh

--- chimera33-setup-amd.sh
+++ chimera33-setup-amd.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
+#retreving prebuilt binary
+sudo curl -O ~/gamescope.zip https://github.com/ruineka/gamescope-onexplayer/files/9056679/gamescope.zip
+sudo unzip ~/gamescope.zip -d /usr/bin
+#Cleanup
+sudo rm ~/gamescope.zip
 
-sudo pacman -Sy base-devel gcc-libs glibc libcap.so=2-64 libdrm llvm-libs libliftoff.so=0-64 libpipewire-0.3.so=0-64 libvulkan.so=1-64 libwlroots.so=10-64 libx11 libxcomposite libxdamage libxext libxkbcommon.so=0-64 libxrender libxres libxtst libxxf86vm sdl2 vulkan-icd-loader wayland wayland-protocols wlroots xorg-server-xwayland git glslang meson ninja vulkan-headers pixman linux-api-headers mesa udev seatd libinput libxfixes libxcb xcb-util-wm libxi xcb-util-errors libglvnd xorgproto
-git clone -b another-experiment https://github.com/ruineka/gamescope-onexplayer ~/gamescope-onexplayer/oxp-90
-cd ~/gamescope-onexplayer/oxp-90
-meson build/
-ninja -C build/
-sudo meson install -C build/ --skip-subprojects
-killall -9 gamescope 
-sudo cp ~/gamescope-onexplayer/oxp-90/build/gamescope /usr/bin
-sudo cp /usr/local/bin/gamescope /usr/bin
-
+#grabbing HandyGCCS
 git clone https://github.com/ShadowBlip/HandyGCCS.git ~/HandyGCCS
 cd ~/HandyGCCS
 sudo make install
+
+#switching to gamepadui
 chimera-session gamepadui
-reboot
+
