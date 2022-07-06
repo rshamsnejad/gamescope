a1b8a984a7142e12dcfb50d2536172a043022487
ruineka <ruinairas1992@gmail.com>
2022-07-06 12:10:29 -0500

    Update chimera33-setup-amd.sh

--- chimera33-setup-amd.sh
+++ chimera33-setup-amd.sh
@@ -2,6 +2,10 @@
 #retreving prebuilt binary
 sudo pacman -S --noconfirm wget
 sudo wget -O ~/gamescope.zip https://github.com/ruineka/gamescope-onexplayer/files/9056679/gamescope.zip
+
+#backing up original gamescope
+sudo mv /usr/bin/gamescope /usr/bin/gamescope.bak
+
 sudo unzip ~/gamescope.zip -d /usr/bin
 #Cleanup
 sudo rm ~/gamescope.zip
